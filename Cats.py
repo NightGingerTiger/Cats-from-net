871fe699845f0522891329bd1f1759f1d748e40b
Rozaliya <tiger.night.tiger@gmail.com>
2024-11-14 23:33:07 +0300

    Создано окно и виджеты

--- Cats.py
+++ Cats.py
@@ -3,3 +3,19 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+url = 'https://cataas.com/cat'
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
